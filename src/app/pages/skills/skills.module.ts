5bc56c869042643ff9dc8dab7d077e4c3f7e5d3e
Florian <florian.martin63000@gmail.com>
2021-06-05 00:00:45 +0200

    Feat: WIP skills part

--- src/app/pages/skills/skills.module.ts
+++ src/app/pages/skills/skills.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SkillsComponent } from '@skills/components/skills.component';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
-const MATERIALS = [MatDividerModule];
+const MATERIALS = [MatDividerModule, MatProgressBarModule];
 @NgModule({
     declarations: [SkillsComponent],
-    imports: [CommonModule, MatDividerModule],
+    imports: [CommonModule, MATERIALS],
     exports: [SkillsComponent],
 })
 export class SkillsModule {}

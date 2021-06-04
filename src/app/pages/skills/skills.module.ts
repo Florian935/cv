d4bfa27d9f4efc447d5df9396a99957f41821eb7
Florian <florian.martin63000@gmail.com>
2021-06-04 23:56:53 +0200

    Feat: created skills title

--- src/app/pages/skills/skills.module.ts
+++ src/app/pages/skills/skills.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SkillsComponent } from '@skills/components/skills.component';
+import { MatDividerModule } from '@angular/material/divider';
 
+const MATERIALS = [MatDividerModule];
 @NgModule({
     declarations: [SkillsComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, MatDividerModule],
     exports: [SkillsComponent],
 })
 export class SkillsModule {}
